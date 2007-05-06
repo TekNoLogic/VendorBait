84876d772393cc6c77bd6f65176f8d1f9b6daaa7
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-05-06 01:27:04 +0000

    Importing VendorBait
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/VendorBait@364 86fe6d9a-1522-0410-a387-bf9db416f0a0

---
+++ VendorBait.lua
@@ -0,0 +1,27 @@
+
+local model = CreateFrame("Model", nil, QuestRewardScrollFrame)
+model:SetModel"Interface\\Buttons\\UI-AutoCastButton.mdx"
+model:SetSequence(0)
+model:SetSequenceTime(0, 0)
+model:SetScale(1.5)
+model:SetFrameLevel(QuestRewardItem1:GetFrameLevel()+1)
+
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("QUEST_COMPLETE")
+f:SetScript("OnEvent", function()
+	model:Hide()
+	local bestp, besti, unk = 0
+	for i=1,GetNumQuestChoices() do
+		local link, name, _, qty = GetQuestItemLink("choice", i), GetQuestItemInfo("choice", i)
+		local price = GetSellValue(link)
+		if not price then unk = true
+		elseif price > bestp then bestp, besti = price, i end
+	end
+
+	if not unk and besti then
+		model:ClearAllPoints()
+		model:SetAllPoints("QuestRewardItem"..besti.."IconTexture")
+		model:Show()
+	end
+end)
