bc1d63016a57a2cb1a6be18fe18677bd4e0475d2
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-05-17 11:55:48 +0000

    VendorBait - Removed ReqDep, made OptDeps for all addons currently implementing GetSellValue
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/VendorBait@390 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- VendorBait.lua
+++ VendorBait.lua
@@ -1,4 +1,5 @@
 
+local _G = getfevn(0)
 local model = CreateFrame("Model", nil, QuestRewardScrollFrame)
 model:SetModel"Interface\\Buttons\\UI-AutoCastButton.mdx"
 model:SetSequence(0)
@@ -10,13 +11,15 @@ model:SetFrameLevel(QuestRewardItem1:GetFrameLevel()+1)
 local f = CreateFrame("Frame")
 f:RegisterEvent("QUEST_COMPLETE")
 f:SetScript("OnEvent", function()
+	if not _G.GetSellValue then return end
+
 	model:Hide()
 	local bestp, besti, unk = 0
 	for i=1,GetNumQuestChoices() do
 		local link, name, _, qty = GetQuestItemLink("choice", i), GetQuestItemInfo("choice", i)
-		local price = GetSellValue(link)
+		local price = _G.GetSellValue(link)
 		if not price then unk = true
-		elseif (price * qty) > bestp then bestp, besti = (price * qty), i end
+		elseif price > bestp then bestp, besti = price, i end
 	end
 
 	if not unk and besti then
