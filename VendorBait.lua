5f33ecbefd17b834c32e211b8c972e5468c14dd6
Tekkub <tekkub@gmail.com>
2009-07-16 23:18:39 -0600

    Shiney!

--- VendorBait.lua
+++ VendorBait.lua
@@ -1,31 +1,6 @@
 
-local mrsparkle = {}
-local f = CreateFrame("Frame", nil, QuestRewardItem1)
+local f = LibStub("tekShiner").new(QuestRewardItem1)
 f:Hide()
-
-for i=1,4 do
-	local tex = f:CreateTexture(nil, "BACKGROUND")
-	tex:SetTexture("Interface\\ItemSocketingFrame\\UI-ItemSockets")
-	tex:SetTexCoord(0.3984375, 0.4453125, 0.40234375, 0.44921875)
-	tex:SetBlendMode("ADD")
-	tex:SetWidth(13) tex:SetHeight(13)
-	mrsparkle[i] = tex
-end
-
-
-local elapsed = 0
-f:SetScript("OnShow", function() elapsed = 0 end)
-f:SetScript("OnUpdate", function(self, elap)
-	local speed = 2.5
-	elapsed = elapsed + elap
-	if elapsed > speed then elapsed = 0 end
-
-	local distance = self:GetWidth()
-	mrsparkle[1]:SetPoint("CENTER", self, "TOPLEFT", elapsed/speed*distance, 0)
-	mrsparkle[2]:SetPoint("CENTER", self, "BOTTOMRIGHT", -elapsed/speed*distance, 0)
-	mrsparkle[3]:SetPoint("CENTER", self, "TOPRIGHT", 0, -elapsed/speed*distance)
-	mrsparkle[4]:SetPoint("CENTER", self, "BOTTOMLEFT", 0, elapsed/speed*distance)
-end)
 
 
 f:RegisterEvent("QUEST_COMPLETE")
