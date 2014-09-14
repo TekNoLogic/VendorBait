bee9ddd22645929698bbebe427dc222eabe6046a
tekkub :bear:  <tekkub@gmail.com>
2014-09-13 22:12:39 -0600

    Switch to my timer external

--- VendorBait.lua
+++ VendorBait.lua
@@ -1,13 +1,11 @@
 
 local myname, ns = ...
 
-local timeout = CreateFrame("Frame")
-timeout:Hide()
-
 local f = LibStub("tekShiner").new(QuestRewardScrollChildFrame)
 f:Hide()
 
 
+local function retry() f:GetScript("OnEvent")(f) end
 f:RegisterEvent("QUEST_COMPLETE")
 f:RegisterEvent("QUEST_ITEM_UPDATE")
 f:RegisterEvent("GET_ITEM_INFO_RECEIVED")
@@ -16,10 +14,8 @@ f:SetScript("OnEvent", function(self, ...)
 	local bestp, besti = 0
 	for i=1,GetNumQuestChoices() do
 		local link, name, _, qty = GetQuestItemLink("choice", i), GetQuestItemInfo("choice", i)
-		if not link then
-			timeout:Show()
-			return
-		end
+		if not link then return ns.StartTimer(GetTime() + 1, retry) end
+
 		local price = link and select(11, GetItemInfo(link))
 		if not price then return
 		elseif (price * (qty or 1)) > bestp then bestp, besti = (price * (qty or 1)), i end
@@ -35,17 +31,5 @@ f:SetScript("OnEvent", function(self, ...)
 end)
 
 
-local elapsed
-timeout:SetScript("OnShow", function() elapsed = 0 end)
-timeout:SetScript("OnHide", function() f:GetScript("OnEvent")(f) end)
-timeout:SetScript("OnUpdate", function(self, elap)
-	elapsed = elapsed + elap
-	if elapsed < 1 then return end
-	self:Hide()
-end)
-
-if ns.isWOD then
-	if QuestFrameRewardPanel:IsVisible() then f:GetScript("OnEvent")(f) end
-else
-	if QuestInfoItem1:IsVisible() then f:GetScript("OnEvent")(f) end
-end
+if ns.isWOD and QuestFrameRewardPanel:IsVisible() then retry() end
+if not ns.isWOD and QuestInfoItem1:IsVisible() then retry() end
